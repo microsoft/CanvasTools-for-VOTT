dd88028786fd07165d878531d7d20107ec076733
Konstantin Kichinsky <constantin@kichinsky.ru>
2018-11-07 14:55:15 +0300

    Added new module CanvasTools to merge the rest

--- src/canvastools/js/CanvasTools.ts
+++ src/canvastools/js/CanvasTools.ts
@@ -4,10 +4,18 @@ import * as Point2D from "@CTBase/CanvasTools.Base.Point2D";
 import * as Rect from "@CTBase/CanvasTools.Base.Rect";
 import * as Tags from "@CTBase/CanvasTools.Base.Tags";
 
+import * as SelectionTool from "@canvastools/selectiontool";
+import * as RegionTool from "@canvastools/regiontool";
+import * as FilterTool from "@canvastools/filtertool";
+
 export namespace CanvasTools {
     export const Base = {
         Point: Point2D.CanvasTools.Base.Point,
         Rect: Rect.CanvasTools.Base.Rect,
         Tags: Tags.CanvasTools.Base.Tags
     }
+
+    export const Selection = SelectionTool.CanvasTools.Selection;
+    export const Region = RegionTool.CanvasTools.Region;
+    export const Filter = FilterTool.CanvasTools.Filter;
 }
