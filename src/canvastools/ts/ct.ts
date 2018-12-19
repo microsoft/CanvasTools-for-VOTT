123627f84338b9009bcab5f409f7bb587c767b48
Konstantin Kichinsky <constantin@kichinsky.ru>
2018-12-19 16:57:32 +0300

    Updated references to reflect structure changes

--- src/canvastools/ts/ct.ts
+++ src/canvastools/ts/ct.ts
@@ -1,14 +1,16 @@
-import { Toolbar as CTToolbar, ToolbarItemType } from "./CanvasTools/CanvasTools.Toolbar";
-import { RegionsManager } from "./CanvasTools/CanvasTools.RegionsManager";
-import { PointRegion } from "./CanvasTools/CanvasTools.PointRegion";
-import { RectRegion } from "./CanvasTools/CanvasTools.RectRegion";
-import { AreaSelector, SelectionMode } from "./CanvasTools/CanvasTools.Selection";
+import { ToolbarItemType } from "./CanvasTools/Toolbar/CanvasTools.ToolbarIcon";
+import { Toolbar as CTToolbar } from "./CanvasTools/Toolbar/CanvasTools.Toolbar";
+import { RegionsManager } from "./CanvasTools/Region/CanvasTools.Region.RegionsManager";
+import { PointRegion } from "./CanvasTools/Region/Point/CanvasTools.Region.PointRegion";
+import { RectRegion } from "./CanvasTools/Region/Rect/CanvasTools.Region.RectRegion";
+import { AreaSelector, SelectionMode } from "./CanvasTools/Selection/CanvasTools.Selection.AreaSelector";
 import { FilterPipeline, InvertFilter, GrayscaleFilter } from "./CanvasTools/CanvasTools.Filter";
-import { RegionComponent } from "./CanvasTools/CanvasTools.RegionComponent";
+import { RegionComponent } from "./CanvasTools/Region/CanvasTools.Region.RegionComponent";
 import { Rect } from "./CanvasTools/Core/CanvasTools.Rect";
 import { Point2D } from "./CanvasTools/Core/CanvasTools.Point2D";
 import { RegionData } from "./CanvasTools/Core/CanvasTools.RegionData";
-import { TagsDescriptor, Tag } from "./CanvasTools/Core/CanvasTools.Tags";
+import { Tag } from "./CanvasTools/Core/CanvasTools.Tag";
+import { TagsDescriptor } from "./CanvasTools/Core/CanvasTools.TagsDescriptor";
 import { Editor as CTEditor } from "./CanvasTools/CanvasTools.Editor";
 
 import "snapsvg-cjs";
