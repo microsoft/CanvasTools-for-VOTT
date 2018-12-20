2d8995b35b7cbb30450f29cc8a2b31edff9f840c
Konstantin Kichinsky <constantin@kichinsky.ru>
2018-12-20 18:43:30 +0300

    Rolled back to use public properties instead of getters

--- src/canvastools/ts/CanvasTools/Core/Rect.ts
+++ src/canvastools/ts/CanvasTools/Core/Rect.ts
@@ -1,32 +1,24 @@
 import { IResizable } from "../Interface/IResizable";
 
 export class Rect implements IResizable {
-    private rectWidth: number;
-    private rectHeight: number;
-
-    public get width(): number {
-        return this.rectWidth;
-    }
-
-    public get height(): number {
-        return this.rectHeight;
-    }
+    public width: number;
+    public height: number;
 
     constructor(width: number, height: number) {
-        this.rectWidth = 0;
-        this.rectHeight = 0;
+        this.width = 0;
+        this.height = 0;
         this.resize(width, height);
     }
 
     public resize(width: number, height: number): void {
         if (width >= 0 && height >= 0) {
-            this.rectWidth = width;
-            this.rectHeight = height;
+            this.width = width;
+            this.height = height;
         }
     }
 
     public copy(): Rect {
-        return new Rect(this.rectWidth, this.rectHeight);
+        return new Rect(this.width, this.height);
     }
 
     public toString(): string {
