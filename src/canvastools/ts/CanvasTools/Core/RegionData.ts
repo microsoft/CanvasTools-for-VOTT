84182f5ba355a3415a386dc90e39d650725a2535
Konstantin Kichinsky <constantin@kichinsky.ru>
2018-12-20 18:44:55 +0300

    Added new getters/setters,  setPoint method to update specific point, initFrom to init values based on other RegionData object

--- src/canvastools/ts/CanvasTools/Core/RegionData.ts
+++ src/canvastools/ts/CanvasTools/Core/RegionData.ts
@@ -15,23 +15,36 @@ export class RegionData implements IMovable, IResizable {
             [new Point2D(x, y), new Point2D(x + width, y + height)], RegionDataType.Rect);
     }
 
-    public boundRect: Rect;
-    public points: Point2D[];
-
     public get x(): number {
         return this.corner.x;
+    }
+
+    public set x(x: number) {
+        this.move(x, this.y);
     }
 
     public get y(): number {
         return this.corner.y;
     }
 
+    public set y(y: number) {
+        this.move(this.x, y);
+    }
+
     public get width(): number {
-        return this.boundRect.width;
+        return this.regionRect.width;
+    }
+
+    public set width(width: number) {
+        this.resize(width, this.height);
     }
 
     public get height(): number {
-        return this.boundRect.height;
+        return this.regionRect.height;
+    }
+
+    public set height(height: number) {
+        this.resize(this.width, height);
     }
 
     public get area(): number {
@@ -40,20 +53,38 @@ export class RegionData implements IMovable, IResizable {
         if (this.regionType === RegionDataType.Point) {
             area = 1.0;
         } else {
-            area = this.boundRect.width * this.boundRect.height;
+            area = this.regionRect.width * this.regionRect.height;
         }
         return area;
     }
 
+    public get boundRect(): Rect {
+        return this.regionRect.copy();
+    }
+
+    public set boundRect(rect: Rect) {
+        this.resize(rect.width, rect.height);
+    }
+
+    public get points(): Point2D[] {
+        return this.regionPoints.map((p) => p.copy());
+    }
+
+    public set points(points: Point2D[]) {
+        this.setPoints(points);
+    }
+
     public regionType: RegionDataType;
 
-    private corner: Point2D;
+    protected corner: Point2D;
+    protected regionRect: Rect;
+    protected regionPoints: Point2D[];
 
     constructor(x: number, y: number, width: number, height: number, points?: Point2D[], regionType?: RegionDataType) {
         this.corner = new Point2D(x, y);
-        this.boundRect = new Rect(width, height);
+        this.regionRect = new Rect(width, height);
 
-        this.points = (points !== undefined && points !== null) ? points : new Array<Point2D>();
+        this.regionPoints = (points !== undefined && points !== null) ? points : new Array<Point2D>();
         this.regionType = (regionType !== undefined) ? regionType : RegionDataType.Point;
     }
 
@@ -76,7 +107,7 @@ export class RegionData implements IMovable, IResizable {
         const sx = width / this.width;
         const sy = height / this.height;
 
-        this.boundRect.resize(width, height);
+        this.regionRect.resize(width, height);
 
         this.points.forEach((p) => {
             const px = (p.x - this.x) * sx + this.x;
@@ -85,12 +116,43 @@ export class RegionData implements IMovable, IResizable {
         });
     }
 
+    public setPoint(point: Point2D, index: number) {
+        if (index >= 0 && index < this.regionPoints.length) {
+            this.regionPoints[index] = point;
+        }
+
+        // Update region position and size
+        let xmin = Number.MAX_VALUE;
+        let xmax = 0;
+        let ymin = Number.MAX_VALUE;
+        let ymax = 0;
+
+        this.regionPoints.forEach((point) => {
+            if (point.x > xmax) {
+                xmax = point.x;
+            }
+            if (point.x < xmin) {
+                xmin = point.x;
+            }
+            if (point.y > ymax) {
+                ymax = point.y;
+            }
+            if (point.y < ymin) {
+                ymin = point.y;
+            }
+        });
+
+        this.corner.move(xmin, ymin);
+        this.regionRect.resize(xmax - xmin, ymax - ymin);
+    }
+
     public setPoints(points: Point2D[]) {
         let xmin = Number.MAX_VALUE;
         let xmax = 0;
         let ymin = Number.MAX_VALUE;
         let ymax = 0;
 
+        // Update region position and size
         points.forEach((point) => {
             if (point.x > xmax) {
                 xmax = point.x;
@@ -106,9 +168,15 @@ export class RegionData implements IMovable, IResizable {
             }
         });
 
-        this.points = points;
+        this.regionPoints = points;
         this.corner.move(xmin, ymin);
         this.boundRect.resize(xmax - xmin, ymax - ymin);
+    }
+
+    public initFrom(regionData: RegionData) {
+        this.corner = new Point2D(regionData.x, regionData.y);
+        this.regionRect = regionData.boundRect.copy();
+        this.regionPoints = regionData.points.map((p) => p.copy());
     }
 
     public boundToRect(rect: Rect): RegionData {
