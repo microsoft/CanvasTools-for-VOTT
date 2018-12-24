b827789c05ad7bf7c0664b1644b1dc2032f32b65
Konstantin Kichinsky <constantin@kichinsky.ru>
2018-12-25 01:08:42 +0300

    Added docs

--- src/canvastools/ts/CanvasTools/Interface/IBoundable.ts
+++ src/canvastools/ts/CanvasTools/Interface/IBoundable.ts
@@ -1,5 +1,13 @@
 import { IResizable } from "./IResizable";
 
+/**
+ * Describes objects that can be bounded to specified box. 
+ */
 export interface IBoundable<T> {
+    /**
+     * Returns an object bounded to specified `rect` box
+     * @param rect - Bounding `rect` (box)
+     * @returns A new object of specified type `T`
+     */
     boundToRect(rect: IResizable): T;
 }
