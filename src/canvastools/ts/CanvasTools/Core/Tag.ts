235a43fa6e705a6ab339724b7a060b7231db62cc
Konstantin Kichinsky <constantin@kichinsky.ru>
2018-12-24 17:44:40 +0300

    Added docs to Tag, moved ITag to separate file, made public properties readonly

--- src/canvastools/ts/CanvasTools/Core/Tag.ts
+++ src/canvastools/ts/CanvasTools/Core/Tag.ts
@@ -1,14 +1,27 @@
-export interface ITag {
-    colorHue: number,
-    name: string,
-    id?: string
-}
+import { ITag } from "../Interface/ITag";
 
-export class Tag {
+/**
+ * Represents meta-data for a tag
+ * @remarks 
+ * 1. To represent the color Tag class uses only the hue component 
+ * and generates a number of color variations based on that value.
+ * 2. The `Tag` object is *immutable*, all public properties are readonly.
+ */
+export class Tag implements ITag {
+    /**
+     * Creates a new tag based on extracting specific properties from any provided object
+     * @param data - An `ITag` object with `name`, `colorHue` and `id` properties
+     * @returns A new `Tag` object
+     */
     public static BuildFromJSON(data: ITag): Tag {
         return new Tag(data.name, data.colorHue, (data.id === undefined) ? "" : data.id);
     }
 
+    /**
+     * Extracts the hue component from a provided CSS color string
+     * @param color - A CSS-color in "#RRGGBB" format
+     * @returns A hue value for provided color
+     */
     public static getHueFromColor(color: string): number {
         const r = parseInt(color.substring(1, 3), 16) / 255;
         const g = parseInt(color.substring(3, 5), 16) / 255;
@@ -36,43 +49,96 @@ export class Tag {
         return h;
     }
 
-    public colorHue: number;
-    public name: string;
-    public id: string;
+    private tagHue: number;
+    private tagName: string;
+    private tagID: string;
+    /**
+     * The hue-value of the tag's color. *Readonly*
+     */
+    public get colorHue(): number {
+        return this.tagHue;
+    };
+
+    /**
+     * The `name` of the tag. *Readonly*
+     */
+
+    public get name(): string {
+        return this.tagName;
+    }
+    /**
+     * The `id` of the tag. *Readonly*
+     */
+    public get id(): string {
+        return this.tagID;
+    }
 
     private tagColorPure: string = "";
+    private tagColorAccent: string = "";
+    private tagColorHighlight: string = "";
+    private tagColorShadow: string = "";
+    private tagColorNoColor: string = "";
+    private tagColorDark: string = "";
+
+    /**
+     * Returns the pure color variation of the tag's color
+     * @returns String hsl(H, 100%, 50%)
+     */
     public get colorPure(): string {
         if (this.tagColorPure === "") {
-            this.tagColorPure = `hsl(${this.colorHue.toString()}, 100%, 50%)`;
+            this.tagColorPure = `hsl(${this.tagHue.toString()}, 100%, 50%)`;
         }
         return this.tagColorPure;
     }
 
-    private tagColorAccent: string = "";
+    /**
+     * Returns the accent color variation of the tag's color
+     * @returns String hsla(H, 100%, 50%, 0.5)
+     */
     public get colorAccent(): string {
         if (this.tagColorAccent === "") {
-            this.tagColorAccent = `hsla(${this.colorHue.toString()}, 100%, 50%, 0.5)`;
+            this.tagColorAccent = `hsla(${this.tagHue.toString()}, 100%, 50%, 0.5)`;
         }
         return this.tagColorAccent;
     }
-
-    private tagColorHighlight: string = "";
+    
+    /**
+     * Returns the highlight color variation of the tag's color
+     * @returns String hsla(H, 80%, 40%, 0.3)
+     */
     public get colorHighlight(): string {
         if (this.tagColorHighlight === "") {
-            this.tagColorHighlight = `hsla(${this.colorHue.toString()}, 80%, 40%, 0.3)`;
+            this.tagColorHighlight = `hsla(${this.tagHue.toString()}, 80%, 40%, 0.3)`;
         }
         return this.tagColorHighlight;
     }
-
-    private tagColorShadow: string = "";
+    
+    /**
+     * Returns the shadow color variation of the tag's color
+     * @returns String hsla(H, 50%, 30%, 0.2)
+     */
     public get colorShadow(): string {
         if (this.tagColorShadow === "") {
-            this.tagColorShadow = `hsla(${this.colorHue.toString()}, 50%, 30%, 0.2)`;
+            this.tagColorShadow = `hsla(${this.tagHue.toString()}, 50%, 30%, 0.2)`;
         }
         return this.tagColorShadow;
     }
 
-    private tagColorNoColor: string = "";
+    /**
+     * Returns the dark color variation of the tag's color
+     * @returns String hsla(H, 50% 30%, 0.8)
+     */
+    public get colorDark(): string {
+        if (this.tagColorDark === "") {
+            this.tagColorDark = `hsla(${this.tagHue.toString()}, 50%, 30%, 0.8)`;
+        }
+        return this.tagColorDark;
+    }
+    
+    /**
+     * Returns the fully transparent color variation of the tag's color
+     * @returns String hsla(0, 0%, 0%, 0.0)
+     */
     public get colorNoColor(): string {
         if (this.tagColorNoColor === "") {
             this.tagColorNoColor = `rgba(0, 0, 0, 0.0)`;
@@ -80,25 +146,35 @@ export class Tag {
         return this.tagColorNoColor;
     }
 
-    private tagColorDark: string = "";
-    public get colorDark(): string {
-        if (this.tagColorDark === "") {
-            this.tagColorDark = `hsla(${this.colorHue.toString()}, 50%, 30%, 0.8)`;
-        }
-        return this.tagColorDark;
-    }
-
+    /**
+     * Creates a new `Tag` object with specified `name`, `colorHue` and `id`
+     * @param name - `name` of the new tag
+     * @param colorHue - `colorHue` of the new tag
+     * @param id - `id` of the new tag (optional, by default is "")
+     */
     constructor(name: string, colorHue: number, id: string = "") {
-        this.name = name;
-        this.colorHue = colorHue;
-        this.id = id;
+        this.tagName = name;
+        this.tagHue = colorHue;
+        this.tagID = id;
     }
 
+    /**
+     * Creates a copy of this tag
+     * @returns A new `Tag` object with copied properties
+     */
+    public copy(): Tag {
+        return new Tag(this.tagName, this.tagHue, this.tagID);
+    }
+
+    /**
+     * Returns a JSON representation of the tag
+     * @returns An `ITag` object with `name`, `colorHue` and `id` properties
+     */
     public toJSON(): ITag {
         return {
-            name: this.name,
-            colorHue: this.colorHue,
-            id: this.id
+            name: this.tagName,
+            colorHue: this.tagHue,
+            id: this.tagID
         }
     }
 }
---
+++ src/canvastools/ts/CanvasTools/Interface/ITag.ts
@@ -0,0 +1,5 @@
+export interface ITag {
+    colorHue: number,
+    name: string,
+    id?: string
+}
