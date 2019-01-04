49bdd43fc37295f0da58f99a5593586f26e5ef0b
Konstantin Kichinsky <constantin@kichinsky.ru>
2019-01-04 13:59:02 +0200

    Added option to create TagsDescriptor from an array of tags. Null primary is now ok.

--- src/canvastools/ts/CanvasTools/Core/TagsDescriptor.ts
+++ src/canvastools/ts/CanvasTools/Core/TagsDescriptor.ts
@@ -12,7 +12,10 @@ export class TagsDescriptor {
      * @returns A new `TagDescriptor` object
      */
     public static BuildFromJSON(data: ITagsDescriptor): TagsDescriptor {
-        let p = Tag.BuildFromJSON(data.primary);
+        let p = null;
+        if (data.primary !== null && data.primary !== undefined) {
+            Tag.BuildFromJSON(data.primary);
+        }
         let s = (data.secondary === undefined) ? [] : data.secondary.map((tag) => Tag.BuildFromJSON(tag));
 
         return new TagsDescriptor(p, s);
@@ -32,37 +35,82 @@ export class TagsDescriptor {
      * Returns the primary tag. *Readonly*
      */
     public get primary(): Tag {
-        return this.primaryTag.copy();
+        if (this.primaryTag !== null) {
+            return this.primaryTag.copy();
+        } else {
+            return null;
+        }        
     }
 
     /**
      * Returns an array of all secondary tags (no order guaranteed). *Readonly*
      */
     public get secondary(): Tag[] {
-        return this.allTags.filter((tag) => {
-            return (tag.name !== this.primary.name);
-        })
+        if (this.primaryTag !== null) {
+            return this.all.filter((tag) => {
+                return (tag.name !== this.primary.name);
+            })
+        } else {
+            return this.all;
+        }        
     }
 
+    /**
+     * Creates a new `TagDescriptor` object with specified tags
+     * @param tags - A tags array with the `tags[0]` used as `primaryTag`
+     */
+    constructor(tags: Tag[]);
+    constructor(primaryTag: Tag, secondaryTags: Tag[]);
     /**
      * Creates a new `TagDescriptor` object with specified tags
      * @param primaryTag - Primary `Tag` for the descriptor
      * @param secondaryTags - An array of secondary tags (optional)
      */
-    constructor(primaryTag: Tag, secondaryTags: Tag[] = []) {
-        this.allTags = new Array<Tag>(primaryTag, ...secondaryTags);
-        this.primaryTag = primaryTag;
+    constructor(arg1: Tag|Tag[], arg2: Tag[] = []) {
+        if (arg1 instanceof Tag) {
+            // arg1 = primaryTag, arg2 = secondaryTag
+            if (arg2 instanceof Array) {
+                this.allTags = new Array<Tag>(arg1, ...arg2);
+            } else {
+                this.allTags = [arg1];
+            }            
+            this.primaryTag = arg1;
+        } else if (arg1 instanceof Array) {
+            // arg1 = tags, ignore arg2
+            this.allTags = arg1.map((tag) => tag.copy());
+            if (arg1.length > 0) {
+                this.primaryTag = arg1[0];
+            } else {
+                this.primaryTag = null;
+            }
+        } else {
+            // arg1 = null | undefined, ignore
+            if (arg2 instanceof Array) {
+                this.allTags = arg2.map((tag) => tag.copy());
+            } else {
+                this.allTags = [];
+            }
+            this.primaryTag = null;
+        }
     }
 
     /**
-     * Returns a string with a space separated list of tags with primary tag first
+     * Returns a string with a comma separated list of tags with primary tag first (if present)
      */
     public toString(): string {
-        let str = this.primary.name;
+        let str = "";
+        if (this.primaryTag !== null) {
+            str += this.primaryTag.name;
 
-        this.secondary.forEach((tag) => {
-            str += " " + tag.name;
-        });
+            this.secondary.forEach((tag) => {
+                str += ", " + tag.name;
+            });
+        } else {
+            this.secondary.forEach((tag) => {
+                str += ", " + tag.name;
+            });
+            str = str.substring(2, str.length);
+        }
 
         return str;
     }
@@ -71,9 +119,17 @@ export class TagsDescriptor {
      * Returns an `ITagsDescriptor` object with `primary` and `secondary` properties
      */
     public toJSON(): ITagsDescriptor {
-        return {
-            primary: this.primaryTag.toJSON(),
-            secondary: this.secondary.map((tag) => tag.toJSON())
+        if (this.primaryTag !== null) {
+            return {
+                primary: this.primaryTag.toJSON(),
+                secondary: this.secondary.map((tag) => tag.toJSON())
+            }
+        } else {
+            return {
+                primary: null,
+                secondary: this.secondary.map((tag) => tag.toJSON())
+            }
         }
+        
     }
 }
