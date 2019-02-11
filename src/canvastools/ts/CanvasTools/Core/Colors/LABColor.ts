f69f8f4a71dfe88b8f2e0d1f186edd0d3a497d1d
Konstantin Kichinsky <constantin@kichinsky.ru>
2019-02-11 13:48:31 +0300

    Added implementation to color distance

--- src/canvastools/ts/CanvasTools/Core/Colors/LABColor.ts
+++ src/canvastools/ts/CanvasTools/Core/Colors/LABColor.ts
@@ -20,21 +20,43 @@ export class LABColor {
         this.values = [l, a, b];
     }
 
+    public distanceTo(color: LABColor): number {
+        const deltaL = this.values[0] - color.values[0];
+        const deltaA = this.values[1] - color.values[1];
+        const deltaB = this.values[2] - color.values[2];
+        const c1 = Math.sqrt(this.values[1] * this.values[1] + this.values[2] * this.values[2]);
+        const c2 = Math.sqrt(color.values[1] * color.values[1] + color.values[2] * color.values[2]);
+        const deltaC = c1 - c2;
+        let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+        deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+        const sc = 1.0 + 0.045 * c1;
+        const sh = 1.0 + 0.015 * c1;
+        const deltaLKlsl = deltaL / (1.0);
+        const deltaCkcsc = deltaC / (sc);
+        const deltaHkhsh = deltaH / (sh);
+        const i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+        return i < 0 ? 0 : Math.sqrt(i);
+    }
+
     public toArray(): number[] {
         // copy
         return this.values.map((v) => v);
     }
 
     public toXYZ(): XYZColor {
-        let y = (this.l + 16) / 116;
-        let x = this.a / 500 + y;
-        let z = y - this.b / 200;
+        let y = (this.l * 100 + 16) / 116;
+        let x = this.a / 5 + y;
+        let z = y - this.b / 2;
 
         [x, y, z] = [x, y, z].map((v) => {
             const v3 = v * v * v;
-            return (v3 > 0.008856451) ? v : (v - 16 / 116) / 7.787037;
+            return (v3 > 0.008856451) ? v3 : (v - 16 / 116) / 7.787037;
         });
 
         return new XYZColor(x * XYZColor.D65.x, y * XYZColor.D65.y, z * XYZColor.D65.z);
+    }
+
+    public toRGB(): RGBColor {
+        return this.toXYZ().toRGB();
     }
 }
