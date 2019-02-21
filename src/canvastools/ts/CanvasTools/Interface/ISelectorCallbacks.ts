8e1ab8e4dd4c8c681f585949477ed685288cb8b9
Konstantin Kichinsky <constantin@kichinsky.ru>
2019-02-21 22:46:39 +0300

    Extracted function types into named types

--- src/canvastools/ts/CanvasTools/Interface/ISelectorCallbacks.ts
+++ src/canvastools/ts/CanvasTools/Interface/ISelectorCallbacks.ts
@@ -1,5 +1,7 @@
 import { RegionData } from "../Core/RegionData";
 
+export type SelectionNotifyFunction = () => void;
+export type SelectionConfirmFunction = (regionData: RegionData) => void;
 /**
  * Defines a collection of callbacks passed to the `AreaSelector` constructor.
  */
@@ -7,13 +9,13 @@ export interface ISelectorCallbacks {
     /**
      * The callback to be called when a new region selection began.
      */
-    onSelectionBegin: () => void;
+    onSelectionBegin: SelectionNotifyFunction;
 
     /**
      * The callback to be called when a new region is selected.
      * @param regionData - The `RegionData` object describing new region.
      */
-    onSelectionEnd: (regionData: RegionData) => void;
+    onSelectionEnd: SelectionConfirmFunction;
 
     /**
      * The callback to be called when the current selector is locked.
